1f3ae6124c3ce196327e6b49b56b4f16ca2e1322
sumithsrinath <113462732+sumithsrinath@users.noreply.github.com>
2023-04-07 23:38:28 +0530

    Intermidiate
    Routing and Set State

--- src/App.js
+++ src/App.js
@@ -1,22 +1,69 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Home from './pages/Home';
+import { BrowserRouter as Router, Route,Switch} from 'react-router-dom';
+
+import About from './pages/About';
+import Contact from './pages/Contact';
+
+//To import the useState
+import { useState } from 'react';
+
+
 
 
 // import PartOne from './components/PartOne';
 
 function App() {
-  return (
-    <div className="App">
 
-      {/* <h1>This is my 1st react project</h1> */}
-      {/* <PartOne/> */}
+  let [count,setCount]=useState(0);
+
+ 
+
+  const incrememnt =()=>{
+    setCount(count+1)
+    
+  }
+
+  const decrement =()=>{
+    setCount(count-1)
+  }
+  
+  return (
+    //Set state
+    <>
+    <span className='titile'> My Counter</span>
+    <p className='subtitle'>The count is {count}</p>
+    <button className='button' onClick={incrememnt}>+</button>
+    <button className='button' onClick={decrement}>-</button>
+
+    </>
+    
+
+
+
+    // <Router>
+    //  <div>
       
-      <Home/>
+    //     <Switch>
+
+    //       <Route exact path="/">
+    //         <Home/>
+    //       </Route>
+
+    //       <Route path="/About">
+    //         <About/>
+    //       </Route>
+
+    //       <Route path="/Contact">
+    //         <Contact/>
+    //       </Route>
+
+
+    //     </Switch>
       
-      
-      
-    </div>
+    //   </div> 
+    // </Router>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,32 @@
 import React from 'react'
-import TopicBox from '../components/TopicBox'
+// import TopicBox from '../components/TopicBox'
+
 
 export default function Home() {
   return (
+
+
+
+
+
+
+
     <div>
       <h1>Homepage</h1>
-      <TopicBox food='Kottu' price='950/='>
+
+
+
+
+
+      {/* <TopicBox food='Kottu' price='950/='>
         <span>This is the description</span>
         </TopicBox>
-        
+
       <TopicBox food='Rice' price='1000/='>
       <button type='submit'>Save</button>
       </TopicBox>
 
-      <TopicBox food='Hoppers' price='300/='/>
+      <TopicBox food='Hoppers' price='300/='/> */}
     </div>
-  )
+  );
 }
